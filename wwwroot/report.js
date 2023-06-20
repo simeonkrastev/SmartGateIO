d4050e46ebce9be29c6ff7001e2314631e3e6ecd
Veselin Aleksiev <veselin.aleksiev.highschool@svetlina.net>
2023-06-20 16:02:39 +0200

    Fixed - Table in reports page #7

--- wwwroot/report.js
+++ wwwroot/report.js
@@ -13,12 +13,27 @@
         var result = JSON.parse(request.response);
 
         result.forEach(checkinData => {
-            var text = "Tag: " + checkinData['rfidTag'] + ", Date: " + checkinData['date'];
+            //var text = "Tag: " + checkinData['rfidTag'] + ", Date: " + checkinData['date'];
 
-            var textNode = document.createTextNode(text);
-            var listItem = document.createElement('li');
-            listItem.appendChild(textNode);
-            content.appendChild(listItem);
+            //var textNode = document.createTextNode(text);
+            //var listItem = document.createElement('li');
+            //listItem.appendChild(textNode);
+            //content.appendChild(listItem);
+            var reportTable = document.getElementById("reportTable");
+            if (tag.Value != reportTable.Value) {
+                var accountName = checkinData['rfidTag'];
+                var dataCheckin = checkinData['data'];
+
+                var firstItem = document.createTextNode(accountName);
+                var firstItemResult = document.createElement('td');
+                firstItemResult.appendChild(firstItem);
+                content.appendChild(firstItemResult);
+
+                var secondItem = document.createTextNode(dataCheckin);
+                var secondItemResult = document.createElement('td');
+                secondItemResult.appendChild(secondItem);
+                content.appendChild(secondItemResult);
+            }
         })
     }
 }
