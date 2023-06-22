094dd17ca922b4ae68dba6b4c642cef3e7016e39
Veselin Aleksiev <veselin.aleksiev.highschool@svetlina.net>
2023-06-22 12:25:05 +0200

    Added table filter

--- wwwroot/report.js
+++ wwwroot/report.js
@@ -14,6 +14,8 @@
 
         result.forEach(checkinData => {
 
+            //table
+
             var accountName = checkinData['rfidTag'];
             var dateCheckin = checkinData['date'];
             var direction = checkinData['status']; 
@@ -43,9 +45,26 @@
             } else {
                 directionCell.style.color = "red";
             }
-
             tableBody.appendChild(newRow);
-
         })
+    }
+}
+    //tableFilter
+function filterTable() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("input");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("reportTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
     }
 }
