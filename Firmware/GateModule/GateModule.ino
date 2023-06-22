17585ea9d6b37a8d853cd6f191e68396e85c4175
FilipNedelchev <filnedelchev@gmail.com>
2023-06-22 13:38:07 +0300

    Kris's changes

--- Firmware/GateModule/GateModule.ino
+++ Firmware/GateModule/GateModule.ino
@@ -55,53 +55,60 @@ void setup() {
 }
 
 void loop() {
-  
-  SetLedPower(2)
-  // //if((millis() - lastTime) > timerDelay) { //Used to count 5 seconds between each request
-  //   //if(WiFi.status() == WL_CONNECTED){
-  //     //SetLedPower(0);
-  //     //if( ! rfid.PICC_IsNewCardPresent())
-  //      //SetLedPower(1);  // Check if a card is near the sensor.
-  //       //return; // If no card is close - do nothing.
+  if((millis() - lastTime) > timerDelay) { //Used to count 5 seconds between each request
+    if(WiFi.status() == WL_CONNECTED){
 
-  //    // if( ! rfid.PICC_ReadCardSerial()) {  // Try to read the card.
-  //      // return;
-  //       //} // If card cannot be read - do nothing.
-  //     //else {
-  //       //SetLedPower(2);
-  //      //}
-  //     // From this point on the rfid variable stores the current card tag.
-  //     //Serial.print("Card read: ");
-  //     // Extract the tag from the rfid variable.
-  //     // String tag = "";
-  //     for(byte i = 0; i < 4; i++) {
-  //       tag += rfid.uid.uidByte[i];
-  //     }
-  //     Serial.println(tag);
+      if( ! rfid.PICC_IsNewCardPresent()) // Check if a card is near the sensor.
+        return; // If no card is close - do nothing.
 
-  //     // Init the http client to send the tag to the backend.
-  //     WiFiClient client;
-  //     HTTPClient http;
-  //     http.begin(client, serverName);
-  //     http.addHeader("Content-Type", "application/json");
+      if( ! rfid.PICC_ReadCardSerial())   // Try to read the card.
+        return; // If card cannot be read - do nothing.
 
-  //     String requestBody = "\"" + tag + "\""; // The request body needs to be surrounded by quotes.
-  //     int responseCode = http.POST(requestBody);
+      String tag = ReadTag();
 
-  //     if(responseCode > 0) {
-  //       Serial.print("HTTP Response code: ");
-  //       Serial.println(responseCode);
-  //     } else {
-  //       Serial.printf("HTTP error: %s\n", http.errorToString(responseCode).c_str());
-  //     }
-
-  //     // Close the HTTP connection.
-  //     http.end();
-  //     // Reset the RFID sensor.
-  //     rfid.PICC_HaltA();
-  //     rfid.PCD_StopCrypto1();
-  //   }
-  //   lastTime = millis();
+      SendRequest (tag)
+      // Reset the RFID sensor.
+      rfid.PICC_HaltA();
+      rfid.PCD_StopCrypto1();
+    }
+    lastTime = millis();
   }
 
 }
+String ReadTag()
+{
+  // From this point on the rfid variable stores the current card tag.
+      Serial.print("Card read: ");
+      // Extract the tag from the rfid variable.
+      String tag = "";
+      for(byte i = 0; i < 4; i++) {
+        tag += rfid.uid.uidByte[i];
+      }
+      Serial.println(tag);
+      return tag;
+}
+
+void SendRequest(String tag)
+{
+  // Init the http client to send the tag to the backend.
+      WiFiClient client;
+      HTTPClient http;
+      http.begin(client, serverName);
+      http.addHeader("Content-Type", "application/json");
+
+      String requestBody = "\"" + tag + "\""; // The request body needs to be surrounded by quotes.
+      int responseCode = http.POST(requestBody);
+
+      if(responseCode > 0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(responseCode);
+      } else {
+        Serial.printf("HTTP error: %s\n", http.errorToString(responseCode).c_str());
+      }
+
+      // Close the HTTP connection.
+      http.end();
+}
+
+
+
