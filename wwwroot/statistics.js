fd95ef9b6e4d409f0a50a3bb2802e65f147390a6
K1ngKRool <ivanstambi@gmail.com>
2023-06-23 14:28:59 +0300

    Made drop down work with edn point

---
+++ wwwroot/statistics.js
@@ -0,0 +1,27 @@
+﻿
+
+window.onload = function loadReportData() {
+    console.log('Loading report data...');
+
+    const url = apiAddress + '/api/accounts';
+    const request = new XMLHttpRequest();
+
+    request.open('GET', url);
+    request.send();
+
+    request.onload = (event) => {
+        var result = JSON.parse(request.response);
+
+        var selectElement = document.getElementById('names');
+
+        // Clear existing options
+        selectElement.innerHTML = '';
+
+        result.forEach(personData => {
+            var option = document.createElement('option');
+            option.value = personData['name'];
+            option.text = personData['name'];
+            selectElement.add(option);
+        })
+    }
+}
