a306eca332d291492423f9728a68f8ad163421b5
Veselin Aleksiev <veselin.aleksiev.highschool@svetlina.net>
2023-06-23 21:54:04 +0300

    1. Moved statistics page before the About page; 2. Fixed: Date Filter Selector; 3. Fixed: Direction Status; 4. Added: Direction Filter; 5. Added: Validation Filter.

--- wwwroot/report.js
+++ wwwroot/report.js
@@ -19,11 +19,12 @@
 
             var accountName = checkinData['name'];
             var dateCheckin = checkinData['date'];
-            var direction = checkinData['status']; 
+            var direction = checkinData['direction']; 
             var validation = checkinData['validationStatus']; 
             var tableBody = document.getElementById('tableBody');
 
             var newRow = document.createElement('tr');
+            newRow.classList.add('data-row');
 
             var accountNameCell = document.createElement('td');
             var accountNameText = document.createTextNode(accountName);
@@ -59,10 +60,10 @@
                     validationCell.style.color = "red";
             }
 
-            if (direction === "IN") {
+            if (direction === "Going In") {
                 directionCell.style.color = "green";
-            } else if (direction === "OUT") {
-                directionCell.style.color = "darkyellow";
+            } else if (direction === "Going Out") {
+                directionCell.style.color = "goldenrod";
             } else {
                 directionCell.style.color = "red";
             }
@@ -71,6 +72,7 @@
         })
     }
 }
+
     //tableFilter
 function filterTable() {
     var input, filter, table, tr, td, i, txtValue;
@@ -94,14 +96,55 @@ function filterTable() {
 function filterTableDate() {
     var input, filter, table, tr, td, i;
     input = document.getElementById("dateInput");
-    filter = input.value;
+    filter = new Date(input.value);
+    table = document.getElementById("reportTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            var cellDate = new Date(td.innerText || td.textContent);
+            var cellDateString = cellDate.toISOString().split("T")[0]; 
+            var filterDateString = filter.toISOString().split("T")[0];
+
+            if (cellDateString <= filterDateString) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function filterTableDirection() {
+    var select, filter, table, tr, td, i;
+    select = document.getElementById("directionFilter");
+    filter = select.value;
     table = document.getElementById("reportTable");
     tr = table.getElementsByTagName("tr");
 
     for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[0];
+        td = tr[i].getElementsByTagName("td")[2];
         if (td) {
-            if (td.innerHTML <= filter) {
+            if (filter === "" || td.innerHTML === filter) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function filterTableValidation() {
+    var select, filter, table, tr, td, i;
+    select = document.getElementById("validationFilter");
+    filter = select.value;
+    table = document.getElementById("reportTable");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[3];
+        if (td) {
+            if (filter === "" || td.innerHTML === filter) {
                 tr[i].style.display = "";
             } else {
                 tr[i].style.display = "none";
