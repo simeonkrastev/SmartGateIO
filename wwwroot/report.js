93d2de949706f85c1735891e13bc912ab2b3e33d
Veselin Aleksiev <veselin.aleksiev.highschool@svetlina.net>
2023-06-23 15:02:20 +0200

    Added: Table formatting #13

--- wwwroot/report.js
+++ wwwroot/report.js
@@ -2,6 +2,7 @@
     console.log('Loading report data...');
 
     var content = document.getElementById('content');
+    document.getElementById("dateInput").addEventListener("change", filterTableDate);
     
     const url = apiAddress + '/api/report/';
     const request = new XMLHttpRequest();
@@ -19,6 +20,7 @@
             var accountName = checkinData['name'];
             var dateCheckin = checkinData['date'];
             var direction = checkinData['status']; 
+            var validation = checkinData['validationStatus']; 
             var tableBody = document.getElementById('tableBody');
 
             var newRow = document.createElement('tr');
@@ -38,6 +40,25 @@
             directionCell.appendChild(directionText);
             newRow.appendChild(directionCell);
 
+                if (validation == true) {
+                    validation = "Valid";
+                    var validationCell = document.createElement('td');
+                    var validationText = document.createTextNode(validation);
+                    validationCell.appendChild(validationText);
+                    newRow.appendChild(validationCell);
+                    validation = "";
+                    validationCell.style.color = "green";
+                }
+                else if (validation == false) {
+                    validation = "Not Valid";
+                    var validationCell = document.createElement('td');
+                    var validationText = document.createTextNode(validation);
+                    validationCell.appendChild(validationText);
+                    newRow.appendChild(validationCell);
+                    validation = "";
+                    validationCell.style.color = "red";
+            }
+
             if (direction === "IN") {
                 directionCell.style.color = "green";
             } else if (direction === "OUT") {
@@ -45,6 +66,7 @@
             } else {
                 directionCell.style.color = "red";
             }
+
             tableBody.appendChild(newRow);
         })
     }
@@ -61,6 +83,25 @@ function filterTable() {
         if (td) {
             txtValue = td.textContent || td.innerText;
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function filterTableDate() {
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("dateInput");
+    filter = input.value;
+    table = document.getElementById("reportTable");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            if (td.innerHTML <= filter) {
                 tr[i].style.display = "";
             } else {
                 tr[i].style.display = "none";
