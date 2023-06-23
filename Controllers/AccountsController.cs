ef18d006522e0f5b880499f30a258fc8367d24a1
KaloyanSavchev <kaloyan.savchev06@gmail.com>
2023-06-23 13:22:37 +0300

    #24
    Create AccountsContoller

---
+++ Controllers/AccountsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartGateIO.Database;
+using SmartGateIO.Models;
+using System.Collections.Generic;
+using static SmartGateIO.Controllers.WebController;
+
+namespace SmartGateIO.Controllers
+{
+    [ApiController]
+    [Route("api/accounts")]
+    public class AccountsContoller
+    {
+        private CheckinsDbContext _context;
+
+        public AccountsContoller(CheckinsDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Account>>> GetAccounts()
+        {
+            return _context.GetAccounts();
+            
+        }
+    }
+}
