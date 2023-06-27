0f95491598a6614237b33977851612f1c0674cf1
FilipNedelchev <filnedelchev@gmail.com>
2023-06-27 14:34:44 +0300

    RGB nad White Led fixed.

--- Firmware/GateModule/GateModule.ino
+++ Firmware/GateModule/GateModule.ino
@@ -7,8 +7,8 @@
 #include <Arduino_JSON.h>
 
 // Represents how the jumper cabels are wired on the board.
-#define SS_PIN D4
-#define RST_PIN D3
+#define SS_PIN D1
+#define RST_PIN D0
 
 MFRC522 rfid(SS_PIN, RST_PIN);
 MFRC522::MIFARE_Key key;
@@ -19,10 +19,10 @@ unsigned long timerDelay = 5000;
 int Led1 = D8;
 
 /*RGBCOLOUR CONST*/
-const int pins[] = { 5, 4, 0 };
-const int red_pin = 5;
-const int green_pin = 4;
-const int blue_pin = 0;
+
+const int red_pin = D3;
+const int green_pin = D2;
+const int blue_pin = D4;
 
 void SetLedPower(int ledPower){
   if(ledPower == 0){
@@ -37,13 +37,14 @@ void SetLedPower(int ledPower){
   if(ledPower == 2){
    
     analogWrite(Led1, 255);
+
   
   }
 }
 void setup() {
-  for (int pin : pins) {
-    pinMode(pin, OUTPUT);
-  }
+  pinMode(red_pin, OUTPUT);
+  pinMode(green_pin, OUTPUT);
+  pinMode(blue_pin, OUTPUT);
   pinMode(Led1, OUTPUT);
   
   // Connect to the USB cable
@@ -72,27 +73,34 @@ void setup() {
 void loop() {
   if((millis() - lastTime) > timerDelay) { //Used to count 5 seconds between each request
     if(WiFi.status() == WL_CONNECTED){
+       SetLedPower(1);
+      if(  !rfid.PICC_IsNewCardPresent())
 
-      if( ! rfid.PICC_IsNewCardPresent()) // Check if a card is near the sensor.
+      // Check if a card is near the sensor.
         return; // If no card is close - do nothing.
-
-      if( ! rfid.PICC_ReadCardSerial())   // Try to read the card.
-        return; // If card cannot be read - do nothing.
-
+      
+      if( !rfid.PICC_ReadCardSerial())   // Try to read the card.
+        return;
+        SetLedPower(2);
+         // If card cannot be read - do nothing.
+      
       String tag = ReadTag();
     
       String response = SendRequest (tag);
- 
+        
         JSONVar jsonResp = JSON.parse(response);
-
+  
         bool validation = jsonResp["validation"];
+      
         if(validation == true){
           Serial.println("Card valid");
          rgbColor("green");
+         
        }
        else if( validation == false){
          Serial.println("Card Invalid");
           rgbColor("red");
+         
        }
        else{
          rgbColor("off");
@@ -132,13 +140,14 @@ String SendRequest(String tag)
   if(responseCode > 0) {
     Serial.print("HTTP Response code: ");
     Serial.println(responseCode);
-  } else {
+  } 
+  else {
     Serial.printf("HTTP error: %s\n", http.errorToString(responseCode).c_str());
   }
-  
+  String result = http.getString();
   // Close the HTTP connection.
   http.end();
-    return http.getString();
+    return result;
 }
 void rgbColor(String color)
 {
@@ -147,12 +156,14 @@ void rgbColor(String color)
     digitalWrite(red_pin, HIGH);
     digitalWrite(blue_pin, LOW);
     digitalWrite(green_pin, LOW);
+    delay(20);
   } 
   else if (color == "green")
   {
     digitalWrite(red_pin, LOW);
     digitalWrite(blue_pin, LOW);
     digitalWrite(green_pin, HIGH);
+    delay(20);
   }
 
   else if (color == "off")
