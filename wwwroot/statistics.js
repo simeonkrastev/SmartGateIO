ea00cc5c510fc7c31a980a917296b4194f33539b
K1ngKRool <ivanstambi@gmail.com>
2023-06-28 12:52:53 +0300

    Made tables work

--- wwwroot/statistics.js
+++ wwwroot/statistics.js
@@ -20,22 +20,67 @@
 
             result.forEach(personData => {
                 var option = document.createElement('option');
-                option.value = personData['name'];
+                option.value = personData['id'];
                 option.text = personData['name'];
                 selectElement.add(option);
             })
 
             // Update the status when the selected option changes
             selectElement.onchange = function () {
-                var selectedName = selectElement.options[selectElement.selectedIndex].value;
-                var selectedPersonData = result.find(personData => personData['name'] === selectedName);
+                var selectedId = selectElement.options[selectElement.selectedIndex].value;
+                var selectedPersonData = result.find(personData => personData['id'] == selectedId);
                 statusElement.textContent = "Current Status: " + selectedPersonData['status'];
+
+                // Load the report for the selected person
+                loadReport(selectedId);
             }
 
             // Update the status based on the initially selected option
-            var initialSelectedName = selectElement.options[selectElement.selectedIndex].value;
-            var initialSelectedPersonData = result.find(personData => personData['name'] === initialSelectedName);
+            var initialSelectedId = selectElement.options[selectElement.selectedIndex].value;
+            var initialSelectedPersonData = result.find(personData => personData['id'] == initialSelectedId);
             statusElement.textContent = "Current Status: " + initialSelectedPersonData['status'];
+
+            // Load the report for the initially selected person
+            loadReport(initialSelectedId);
+        } else {
+            console.error("Error loading data: " + request.status + " " + request.statusText);
+        }
+    }
+
+    request.onerror = () => {
+        console.error("Request failed");
+    };
+
+    request.send();
+}
+
+function loadReport(id) {
+    const url = apiAddress + '/api/report/Id?Id=' + id;
+    const request = new XMLHttpRequest();
+
+    request.open('GET', url);
+    request.onload = () => {
+        if (request.status == 200) {
+            var result = JSON.parse(request.response);
+
+            var checkInTimesBody = document.getElementById('checkInTimesBody');
+            var checkOutTimesBody = document.getElementById('checkOutTimesBody');
+
+            // Clear any existing rows
+            checkInTimesBody.innerHTML = '';
+            checkOutTimesBody.innerHTML = '';
+
+            result.forEach(record => {
+                var dateParts = record.date.split(' ');
+                var row = document.createElement('tr');
+                row.innerHTML = `<td>${dateParts[0]}</td><td>${dateParts[1]} ${dateParts[2]}</td>`;
+
+                if (record.direction == 'Going In') {
+                    checkInTimesBody.appendChild(row);
+                } else {
+                    checkOutTimesBody.appendChild(row);
+                }
+            });
         } else {
             console.error("Error loading data: " + request.status + " " + request.statusText);
         }
