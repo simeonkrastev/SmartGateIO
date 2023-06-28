05b5ad640757b4b97437a6c3a187abba37586f39
FilipNedelchev <filnedelchev@gmail.com>
2023-06-29 02:15:11 +0300

    Servo Motor test

--- Firmware/GateModule/GateModule.ino
+++ Firmware/GateModule/GateModule.ino
@@ -5,17 +5,20 @@
 #include <MFRC522.h>
 #include "constants.h"
 #include <Arduino_JSON.h>
-
+#include <Servo.h>
 // Represents how the jumper cabels are wired on the board.
 #define SS_PIN D1
 #define RST_PIN D0
 
 MFRC522 rfid(SS_PIN, RST_PIN);
 MFRC522::MIFARE_Key key;
+//Servo myservo ;
+ int servomoto = D9;
+int currentpos = 0;
 
 unsigned long lastTime = 0;
 unsigned long timerDelay = 5000;
-
+int pos = 0;
 int Led1 = D8;
 
 /*RGBCOLOUR CONST*/
@@ -37,7 +40,7 @@ void SetLedPower(int ledPower){
   if(ledPower == 2){
    
     analogWrite(Led1, 255);
-
+    delay(1);
   
   }
 }
@@ -46,7 +49,8 @@ void setup() {
   pinMode(green_pin, OUTPUT);
   pinMode(blue_pin, OUTPUT);
   pinMode(Led1, OUTPUT);
-  
+ // myservo.attach(D9);
+   pinMode(servomoto, OUTPUT);
   // Connect to the USB cable
   Serial.begin(115200);
   Serial.println("Serial ready!");
@@ -71,9 +75,13 @@ void setup() {
 }
 
 void loop() {
+  
+
   if((millis() - lastTime) > timerDelay) { //Used to count 5 seconds between each request
     if(WiFi.status() == WL_CONNECTED){
        SetLedPower(1);
+        rgbColor("off");
+ 
       if(  !rfid.PICC_IsNewCardPresent())
 
       // Check if a card is near the sensor.
@@ -95,16 +103,15 @@ void loop() {
         if(validation == true){
           Serial.println("Card valid");
          rgbColor("green");
-         
+         rotateForward();
+        
        }
        else if( validation == false){
          Serial.println("Card Invalid");
           rgbColor("red");
-         
+
        }
-       else{
-         rgbColor("off");
-       }
+       
       // Reset the RFID sensor.
       rfid.PICC_HaltA();
       rfid.PCD_StopCrypto1();
@@ -156,14 +163,14 @@ void rgbColor(String color)
     digitalWrite(red_pin, HIGH);
     digitalWrite(blue_pin, LOW);
     digitalWrite(green_pin, LOW);
-    delay(20);
+    delay(1);
   } 
   else if (color == "green")
   {
     digitalWrite(red_pin, LOW);
     digitalWrite(blue_pin, LOW);
     digitalWrite(green_pin, HIGH);
-    delay(20);
+    
   }
 
   else if (color == "off")
@@ -174,6 +181,36 @@ void rgbColor(String color)
 
   }
 }
-
+int pos1 = 0;
+void moveTo(int position){
+  // if(position> pos){
+  //     for(pos = pos1; pos <= position; pos += 1){
+  //       analogWrite(servomoto, pos);
+  //       pos1 = pos;
+  //       delay(40);
+  //       }
+  //    }
+    //  else{
+    //   for(pos = pos1; pos>= position; pos -=1){
+    //     myservo.write(pos);
+    //     pos1 = pos;
+    //     delay(40);
+    //     }
+    //   }
+}
+void rotateForward(){
+// analogWrite(servomoto, 120);
+// delay(5);
+         for (pos = 0; pos <= 120; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+     analogWrite(servomoto, pos);
+    //myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(60);                       // waits 15ms for the servo to reach the position
+  }                     //   in steps of 1 degree
+//  currentpos += 120;
+//      moveTo(currentpos);             // tell servo to go to position in variable 'pos'
+   
+ 
+}
 
 
